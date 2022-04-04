ebaf20d908710cf09b46d114d31c3ab0d2bc93d9
urmila4462 <dasari.urmila4462@gmail.com>
2022-04-04 18:19:03 +0530

    [urmila]ADDED implmented attendance problem in emp wage problme

---
+++ empwage.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+
+        Employee=$((RANDOM%2))
+
+
+         if [ $Employee -eq 1 ];
+         then
+         echo "Employee is Present" $Employee
+         else
+         echo "Employee is Absent" $Employee
+         fi
+
+
+
+
