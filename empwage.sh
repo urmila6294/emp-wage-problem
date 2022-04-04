81359d9d4658e80165fa1481c4ed1caff219d1d2
urmila4462 <dasari.urmila4462@gmail.com>
2022-04-04 18:54:16 +0530

    [urmila]ADDED implemented switch case in emp wage problem

--- empwage.sh
+++ empwage.sh
@@ -4,7 +4,7 @@
         Employee=$((RANDOM%2))
 
 
-         if [ $Employee -eq 1 ];
+      	 if [ $Employee -eq 1 ];
          then
          echo "Employee is Present" $Employee
          else
@@ -17,5 +17,15 @@ Daily_Emp_Wage=$(($Wage_Per_Hr*$Full_Day_Hr))
 echo "Daily Emp Wage is:" $Daily_Emp_Wage
 
 Parttime_Hr=4
+        
+        case $Employee in
+            1)echo "Daily Employee Wage Full Time - "$DailyEmpWage=$(($Full_Day_Hr
+*$Wage_Per_Hr)) ;;
+            
+		 0)echo "Daily Employee Wage part Time - "$DailyEmpWage=$(($Parttime_Hr
+*$Wage_Per_Hr)) ;;
+
+            *)echo "wrong choice";;
+        esac
 
 
